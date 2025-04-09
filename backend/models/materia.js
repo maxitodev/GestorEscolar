27d1ebe3e3477496a713914acd9787562a21e393
Maxuriel <max.sanchezd13@gmail.com>
2025-04-08 19:14:39 -0600

    Correción de Tabla materias

--- backend/models/materia.js
+++ backend/models/materia.js
@@ -3,7 +3,7 @@ const db = require('../config/db');
 // Crear una nueva materia con nombre, carrera y capacidad
 const crearMateria = (nombre, carreraFK, capacidad, callback) => {
   const safeNombre = typeof nombre === 'string' ? nombre : String(nombre);
-  const sql = `INSERT INTO materia (nombre_materia, carrera_FK, capacidad) VALUES (?, ?, ?)`;
+  const sql = `INSERT INTO Materia (nombre_materia, carrera_FK, capacidad) VALUES (?, ?, ?)`;
   db.query(sql, [safeNombre, carreraFK, capacidad], (err, result) => {
     if (err) return callback(err);
     callback(null, result);
@@ -12,7 +12,7 @@ const crearMateria = (nombre, carreraFK, capacidad, callback) => {
 
 // Obtener todas las materias con el nombre de la carrera asociada
 const obtenerMaterias = (callback) => {
-  const sql = `SELECT * FROM materia INNER JOIN carreras ON materia.carrera_FK = carreras.ID_carrera`;
+  const sql = `SELECT * FROM Materia INNER JOIN Carreras ON Materia.carrera_FK = Carreras.ID_carrera`;
 
   db.query(sql, (err, results) => {
     if (err) return callback(err);
